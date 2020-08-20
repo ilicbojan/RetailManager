51a90e71dc993e0a90933cf516b660525206c348
ilicbojan <bojan.ilic2404@gmail.com>
2020-08-20 17:31:57 +0200

    Wire Up Login Form to API
    Connects the login form button to the authentication API endpoint (/token). Gets back the bearer token or an exception if failed

---
+++ RMDesktopUI/ViewModels/LoginViewModel.cs
@@ -0,0 +1,74 @@
+﻿using Caliburn.Micro;
+using RMDesktopUI.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RMDesktopUI.ViewModels
+{
+    public class LoginViewModel : Screen
+    {
+        private string _username;
+        private string _password;
+        private readonly IAPIHelper _apiHelper;
+
+        public LoginViewModel(IAPIHelper apiHelper)
+        {
+            this._apiHelper = apiHelper;
+        }
+
+        public string Username
+        {
+            get { return _username; }
+            set
+            {
+                _username = value;
+                NotifyOfPropertyChange(() => Username);
+                NotifyOfPropertyChange(() => CanLogIn);
+            }
+        }
+
+        public string Password
+        {
+            get { return _password; }
+            set
+            {
+                _password = value;
+                NotifyOfPropertyChange(() => Password);
+                NotifyOfPropertyChange(() => CanLogIn);
+            }
+        }
+
+        public bool CanLogIn
+        {
+            get
+            {
+                bool output = false;
+
+                if (Username?.Length > 0 && Password?.Length > 0)
+                {
+                    output = true;
+                }
+
+                return output;
+            }
+        }
+
+        public async Task LogIn()
+        {
+            try
+            {
+                var result = await _apiHelper.Authenticate(Username, Password);
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+
+
+        }
+    }
+}
